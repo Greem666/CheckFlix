72cada8a70c4d875483f3b7025f30ed730c0e4d4
Greem666 <29668023+Greem666@users.noreply.github.com>
2020-12-02 09:58:45 +1100

    All works, before email and scheduler integration

--- src/main/java/com/maciej/checkflix/scheduler/ProviderWatchlistScheduler.java
+++ src/main/java/com/maciej/checkflix/scheduler/ProviderWatchlistScheduler.java
@@ -1,19 +1,23 @@
 package com.maciej.checkflix.scheduler;
 
 import com.maciej.checkflix.repository.watchlist.ProviderWatchlistRepository;
+import com.maciej.checkflix.service.WatchlistEmailService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class ProviderWatchlistScheduler {
-    private static final String SUBJECT = "Tasks: Once a day email";
-//    private final SimpleEmailService simpleEmailService;
+    private static final Logger logger = LoggerFactory.getLogger(ProviderWatchlistScheduler.class);
+
+    private final WatchlistEmailService watchlistEmailService;
     private final ProviderWatchlistRepository providerWatchlistRepository;
 
     @Scheduled(cron = "0 */12 * * *")
     public void notifyAboutProviderAvailabilityChange() {
-
+        // logic for this...
     }
 }
--- src/main/java/com/maciej/checkflix/service/WatchlistEmailService.java
+++ src/main/java/com/maciej/checkflix/service/WatchlistEmailService.java
@@ -1,8 +1,10 @@
 package com.maciej.checkflix.service;
 
+import com.maciej.checkflix.domain.mail.Mail;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -12,9 +14,29 @@ import org.springframework.stereotype.Service;
 public class WatchlistEmailService {
     private static final Logger LOGGER = LoggerFactory.getLogger(WatchlistEmailService.class);
 
-    public static final String NEW_CARD_EMAIL = "New card email";
-    public static final String ONCE_A_DAY_EMAIL = "Once a day email";
-
 //    private final JavaMailSender javaMailSender;
-//    private final MailCreatorService mailCreatorService;
+
+//    public void send(final Mail mail) {
+//        LOGGER.info("Starting email preparation...");
+//        try {
+//            javaMailSender.send(createMailMessage(mail));
+//            LOGGER.info("Email has been sent.");
+//        } catch (MailException e) {
+//            LOGGER.error("Failed to process email sending: ", e.getMessage(), e);
+//        }
+//    }
+
+
+    public SimpleMailMessage createMailMessage(final Mail mail) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        // Needed on my PC, as it is not configured to have a fully qualified domain name
+        mailMessage.setFrom("Checkflix providers watchlist");
+
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+
+        return mailMessage;
+    }
 }
